c37dbb958fae7881e95bfb1c2c225f768f835e6a
ISHITA <91796142+Ishita2407@users.noreply.github.com>
2024-07-07 18:04:33 +0530

    Update Flatten LL.java

--- Flatten LL.java
+++ Flatten LL.java
@@ -1,4 +1,43 @@
-                                
+// Brute force approach
+class GfG {
+    public Node convertArrToLL(ArrayList<Integer> arr){
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+        
+        for(int i = 0; i < arr.size(); i++){
+            temp.bottom = new Node(arr.get(i));
+            temp = temp.bottom;
+        }
+        return dummy.bottom;
+    }
+    Node flatten(Node root) {
+        // Brute force approach
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        
+        while(root != null){
+            Node t2 = root;
+            
+            while(t2 != null){
+                arr.add(t2.data);
+                t2 = t2.bottom;
+            }
+            root = root.next;
+        }
+        Collections.sort(arr);
+        return convertArrToLL(arr);
+    }
+}
+Time Complexity: O(N*M) + O(N*M log(N*M)) + O(N*M)where N is the length of the linked list along the next pointer and M is the length of the linked list along the child pointer.
+O(N*M) as we traverse through all the elements, iterating through ‘N’ nodes along the next pointer and ‘M’ nodes along the child pointer.
+O(N*M log(N*M)) as we sort the array containing N*M (total) elements.
+O(N*M) as we reconstruct the linked list from the sorted array by iterating over the N*M elements of the array.
+  
+Space Complexity : O(N*M) + O(N*M)where N is the length of the linked list along the next pointer and M is the length of the linked list along the child pointer.
+O(N*M) for storing all the elements in an additional array for sorting.
+O(N*M) to reconstruct the linked list from the array after sorting
+
+
+// Optimal solution                                
 class Node {
     int data;
     Node next;
@@ -144,5 +183,9 @@ public class FlattenLinkedList {
 }
 
 
-                                
+Time Complexity: O( N*(2M) ) ~ O(2 N*M)where N is the length of the linked list along the next pointer and M is the length of the linked list along the child pointers.
+The merge operation in each recursive call takes time complexity proportional to the length of the linked lists being merged as they have to iterate over the entire lists. Since the vertical depth of the linked lists is assume to be M, the time complexity for a single merge operation is proportional to O(2*M).
+This operation operation is performed N number of times (to each and every node along the next pointer list) hence the resultant time complexity becomes: O(N* 2M).
+
+Space Complexity : O(1) as this algorithm uses no external space or additional data structures to store values. But a recursive stack uses O(N) space to build the recursive calls for each node along the next pointer list.                           
                             
